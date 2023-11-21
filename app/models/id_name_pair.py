4a18d5466681592da28254c8a778c78912abb4da
Tabrez Ahmed <tabrez.tk@gmail.com>
2023-11-21 16:54:32 +0530

    Added created_by to id/name models

--- app/models/id_name_pair.py
+++ app/models/id_name_pair.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, PrimaryKeyConstraint, UniqueConstraint
+from sqlalchemy import Column, PrimaryKeyConstraint, UniqueConstraint, ForeignKeyConstraint
 from sqlalchemy.dialects.postgresql import UUID, CITEXT
+from sqlalchemy.orm import relationship, declared_attr
 from uuid import uuid4
 from .base import BaseModel
 
@@ -8,11 +9,17 @@ def create_id_name_pair_model(table_name: str):
         __tablename__ = table_name
         __abstract__ = True
         id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, nullable=False)
-        name = Column(CITEXT(), nullable=False)    
+        name = Column(CITEXT(), nullable=False)  
+        created_by_user_id = Column(UUID)
+
+        @declared_attr
+        def created_by(cls):
+            return relationship("UserModel", foreign_keys=[cls.created_by_user_id])  
 
         __table_args__ = (
             PrimaryKeyConstraint('id', name=f'pk__{table_name}__id'),
             UniqueConstraint('id', name=f'uq__{table_name}__id'),
             UniqueConstraint('name', name=f'uq__{table_name}__name'),
+            ForeignKeyConstraint(['created_by_user_id'], ['users.id'], name=f'fk__{table_name}.created_by__users.id')
         )
     return IDNamePairModel
